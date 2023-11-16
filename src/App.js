f2a58b43234f95c56150be433d33224105c771b6
polyhistor2050 <chrisspaul2050@gmail.com>
2023-11-16 09:36:29 +0300

    Modify app component

--- src/App.js
+++ src/App.js
@@ -58,30 +58,15 @@ class App extends Component {
         const { searchTerm, list } = this.state;
         return (
             <div>
-                <form>
-                    <input
-                     type="text"
-                     value={searchTerm}
-                     onChange={this.onSearchChange}
-                    />
-                </form>
-                {this.state.list.filter(isSeached(searchTerm)).map(item => 
-                    <div key={item.objectID}>
-                        <span><a href={item.url}>{item.title}</a></span>
-                        <p>{item.author}</p>
-                        <p>{item.num_comments}</p>
-                        <p>{item.points}</p>
-                        <span>
-                            <button
-                                onClick={() => this.onDismiss(item.objectID)}
-                                type='button'
-                            >
-                                Dismiss
-                            </button>
-                        </span>
-                    </div>
-                    
-                )}
+                <Search 
+                    value={searchTerm}
+                    onChange={this.onSearchChange}
+                />
+                <Table 
+                    list={list}
+                    pattern={searchTerm}
+                    onDismiss={this.onDismiss}
+                />
             </div>
         )
     }
