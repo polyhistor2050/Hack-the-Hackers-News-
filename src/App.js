a44a6266ee152bd2ecf7830406c59859e6b36650
polyhistor2050 <chrisspaul2050@gmail.com>
2023-11-16 09:21:49 +0300

    Split app to search and table components

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ class App extends Component {
                 <form>
                     <input
                      type="text"
+                     value={searchTerm}
                      onChange={this.onSearchChange}
                     />
                 </form>
@@ -85,6 +86,53 @@ class App extends Component {
         )
     }
 
+}
+
+
+class Search extends Component {
+    render() {
+        const { value, onChange } = this.props;
+        return(
+            <form>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+            </form>
+        );
+    }
+}
+
+
+class Table extends Component {
+    render() {
+        const { list, pattern, onDismiss } = this.props;
+        return(
+            <div>
+                {list.filter(isSeached(pattern)).map(item => {
+                    return(
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <p>{item.author}</p>
+                            <p>{item.num_comments}</p>
+                            <p>{item.points}</p>
+                            <span>
+                                <button 
+                                    type='button'
+                                    onClick={() => onDismiss(item.objectID)}
+                                >
+                                    Dismiss
+                                </button>
+                            </span>
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
