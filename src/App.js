daa1f3e14e138150626be2e6829a15754e39f4de
polyhistor2050 <chrisspaul2050@gmail.com>
2023-11-24 17:47:15 +0300

    Extends the API endpoint to fetch more list items

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import './App.css';
 
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
 const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
 
 
 class App extends Component {
@@ -50,7 +53,7 @@ class App extends Component {
     }
 
     fetchSearchTopStories(searchTerm, page = 0) {
-        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
             .then(response => response.json())  // transform response to json formatt
             .then(result => this.setSearchTopStories(result))
             .catch(error => error);
